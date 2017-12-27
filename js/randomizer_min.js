cc00a13ae54ff9caf59c60a2167356b74dfe5a52
DRFire9001 <kallan.sipple@hotmail.com.br>
2017-12-27 12:52:47 -0200

    background code update

--- js/randomizer_min.js
+++ js/randomizer_min.js
@@ -1,22 +1,20 @@
-
-			var variable = [`--img1`, `--img2`, `--img3`, `--img4`, `--img5`, `--img6`];
-			var images = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg'];
-			
-			for ( i = 0; i < variable.length; i++){
-				var imgs= images[Math.floor(Math.random() * images.length)];
-				if (window.innerWidth > 600){
-					document.documentElement.style.setProperty(variable[i], 'url(../images/' + imgs + ')' );
-				}
-				else{
-					document.documentElement.style.setProperty(variable[i], 'url(../images/phone/' + imgs + ')' );
-				}
-				remove(images, imgs);
-			}
-		
-			function remove(array, element) {
-				const index = array.indexOf(element);
-    
-				if (index !== -1) {
-					array.splice(index, 1);
-				}
-			}
+function scramble(){
+	var variable = [`--img1`, `--img2`, `--img3`, `--img4`, `--img5`, `--img6`];
+	var images = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg'];
+	for ( i = 0; i < variable.length; i++){
+		var imgs= images[Math.floor(Math.random() * images.length)];
+		if (window.innerWidth > 600){
+			document.documentElement.style.setProperty(variable[i], 'url(../images/' + imgs + ')' );
+		}
+		else{
+			document.documentElement.style.setProperty(variable[i], 'url(../images/phone/' + imgs + ')' );
+		}
+		remove(images, imgs);
+	}	
+}
+function remove(array, element) {
+	const index = array.indexOf(element);
+	if (index !== -1) {array.splice(index, 1);}
+}		
+$(document).ready(function () {scramble();//run when page first loads});
+$(window).resize(function () {scramble();//run on every window resize});
