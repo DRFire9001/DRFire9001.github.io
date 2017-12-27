e1e27fcb7ead5e9f09da2cf701640059a40a6696
DRFire9001 <kallan.sipple@hotmail.com.br>
2017-12-27 01:53:57 -0200

    background update
    attemp at improving dynamic, random background (screen size change).

---
+++ js/randomizer_min.js
@@ -0,0 +1,22 @@
+
+			var variable = [`--img1`, `--img2`, `--img3`, `--img4`, `--img5`, `--img6`];
+			var images = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg'];
+			
+			for ( i = 0; i < variable.length; i++){
+				var imgs= images[Math.floor(Math.random() * images.length)];
+				if (window.innerWidth > 600){
+					document.documentElement.style.setProperty(variable[i], 'url(../images/' + imgs + ')' );
+				}
+				else{
+					document.documentElement.style.setProperty(variable[i], 'url(../images/phone/' + imgs + ')' );
+				}
+				remove(images, imgs);
+			}
+		
+			function remove(array, element) {
+				const index = array.indexOf(element);
+    
+				if (index !== -1) {
+					array.splice(index, 1);
+				}
+			}
